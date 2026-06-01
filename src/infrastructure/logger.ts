c1451ea751fd964e0df9a130e4d066db096fef0f
Juan David Barboza Diaz <D3489213@CorredorEmpresarial.com.co>
2026-06-01 11:44:25 -0500

    chore: The configuration of eslint, prettier and husky is done

--- src/infrastructure/logger.ts
+++ src/infrastructure/logger.ts
@@ -2,6 +2,6 @@ import pino from 'pino';
 import { env } from './config/env.js';
 
 export const logger = pino({
-    level: env.LOG_LEVEL,
-    timestamp: pino.stdTimeFunctions.isoTime,
+  level: env.LOG_LEVEL,
+  timestamp: pino.stdTimeFunctions.isoTime,
 });
